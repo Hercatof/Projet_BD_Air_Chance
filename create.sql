0750a451bf33b130a5111575bd18d0324d809692
pinetq <Quentin.Pinet@etu.upmf-grenoble.fr>
2020-03-31 20:47:01 +0200

    ajout drop et clean
    clean des erreurs d'exécution et ajout de drop

--- create.sql
+++ create.sql
@@ -2,19 +2,19 @@ CREATE TABLE Modele (
     nomModele VARCHAR2(3),
     nbPilotesMin NUMBER(3,0),
     rayonMax NUMBER(7,0),
-    CONSTRAINT cp PRIMARY KEY (nomModele)
+    CONSTRAINT cPrimaryModele PRIMARY KEY (nomModele)
 );
 
 CREATE TABLE Avion (
   numAvion NUMBER(7,0),
   nomModele VARCHAR2(100) NOT NULL,
-  capacite(5,0) NUMBER,
+  capacite NUMBER(5,0),
   nbEconomique NUMBER(4,0),
   nbPremiere NUMBER(4,0),
   nbAffaire NUMBER(4,0),
-  CONSTRAINT cp PRIMARY KEY (numAvion),
-  CONSTRAINT cf FOREIGN KEY (nomModele) REFERENCES Modele(nomModele),
-  CONSTRAINT cCapacite CHECK (capacite = (nbEconomique + nbPremiere + nbAffaire))
+  CONSTRAINT cPrimaryAvion PRIMARY KEY (numAvion),
+  CONSTRAINT cForeignAvion FOREIGN KEY (nomModele) REFERENCES Modele(nomModele),
+  CONSTRAINT cCapaciteAvion CHECK (capacite = (nbEconomique + nbPremiere + nbAffaire))
 );
 
 CREATE TABLE Vol (
@@ -22,15 +22,15 @@ CREATE TABLE Vol (
   numAvion NUMBER(7,0) NOT NULL,
   origine VARCHAR2(100),
   destination VARCHAR2(100),
-  heureDep TIME,
+  heureDep DATE,
   jourDep DATE,
   heureAr TIME,
   jourAr DATE,
   distance NUMBER(6,0),
   volTermine BOOLEAN,
   nbPilotesMin NUMBER(3,0),
-  CONSTRAINT cp PRIMARY KEY (numVol),
-  CONSTRAINT cf FOREIGN KEY (numAvion) REFERENCES Avion(numAvion)
+  CONSTRAINT cPrimaryVol PRIMARY KEY (numVol),
+  CONSTRAINT cForeignVol FOREIGN KEY (numAvion) REFERENCES Avion(numAvion)
 );
 --pour Modele.nomModele = Avion.nomModele et Avion.nomAvion = Vol.nomAvion Modele.nbPilotesMin <= Vol.nbPilotesMin
 
@@ -39,18 +39,18 @@ CREATE TABLE Place (
   numAvion NUMBER(7,0) NOT NULL,
   classe VARCHAR2(100),
   position VARCHAR(7),
-  CONSTRAINT cp PRIMARY KEY (numPlace),
-  CONSTRAINT cf FOREIGN KEY (numAvion) REFERENCES Avion(numAvion),
+  CONSTRAINT cPrimaryPlace PRIMARY KEY (numPlace),
+  CONSTRAINT cForeignPlace FOREIGN KEY (numAvion) REFERENCES Avion(numAvion),
   CONSTRAINT cClasse CHECK (classe IN ('ECONOMIQUE', 'PREMIERE', 'AFFAIRE'),
-  CONSTRAINT cPosition CHECK (position IN ('HUBLOT', 'COULOIR', 'CENTRE')
+  CONSTRAINT cPosition CHECK (position IN ('HUBLOT', 'COULOIR', 'CENTRE'))
 );
 
 CREATE TABLE PlaceVol (
   numVol NUMBER(10,0),
   numPlace NUMBER(5,0),
-  CONSTRAINT cp PRIMARY KEY (numVol,numero_place),
-  CONSTRAINT cf1 FOREIGN KEY (numVol) REFERENCES Vol(numVol),
-  CONSTRAINT cf2 FOREIGN KEY (numPlace) REFERENCES Place(numPlace)
+  CONSTRAINT cPrimaryPlaceVol PRIMARY KEY (numVol,numPlace),
+  CONSTRAINT cForeign1PlaceVol FOREIGN KEY (numVol) REFERENCES Vol(numVol),
+  CONSTRAINT cForeign2PlaceVol FOREIGN KEY (numPlace) REFERENCES Place(numPlace)
 );
 
 
@@ -64,7 +64,7 @@ CREATE TABLE Client (
   adrCodePostal NUMBER(5,0),
   adrVille VARCHAR2(100),
   adrPays VARCHAR2(100),
-  CONSTRAINT cp PRIMARY KEY (numClient)
+  CONSTRAINT cPrimaryClient PRIMARY KEY (numClient)
 );
 
 CREATE TABLE Reservation (
@@ -74,9 +74,9 @@ CREATE TABLE Reservation (
   numPlace NUMBER(5,0) NOT NULL,
   dateReservation DATE,
   prix NUMBER(9,2),
-  CONSTRAINT cp PRIMARY KEY (numReservation),
-  CONSTRAINT cf1 FOREIGN KEY (numClient) REFERENCES Client(numClient),
-  CONSTRAINT cf2 FOREIGN KEY (numVol, numPlace) REFERENCES PlaceVol(numVol, numPlace)
+  CONSTRAINT cPrimaryReservation PRIMARY KEY (numReservation),
+  CONSTRAINT cForeign1Reservation FOREIGN KEY (numClient) REFERENCES Client(numClient),
+  CONSTRAINT cForeign2Reservation FOREIGN KEY (numVol, numPlace) REFERENCES PlaceVol(numVol, numPlace)
 );
 
 CREATE TABLE Pilote (
@@ -89,12 +89,12 @@ CREATE TABLE Pilote (
   adrVille VARCHAR2(100),
   adrPays VARCHAR2(100),
   nbHeuresVolTotal NUMBER(6,0),
-  CONSTRAINT cp PRIMARY KEY (numPilote)
+  CONSTRAINT cPrimaryPilote PRIMARY KEY (numPilote)
 );
 
 CREATE TABLE Langue (
   libLangue VARCHAR2(100),
-  CONSTRAINT cp PRIMARY KEY (libLangue)
+  CONSTRAINT cPrimaryLangue PRIMARY KEY (libLangue)
 );
 
 CREATE TABLE Hotesse (
@@ -107,15 +107,15 @@ CREATE TABLE Hotesse (
   adrVille VARCHAR2(100),
   adrPays VARCHAR2(100),
   nbHeuresVolTotal NUMBER(6,0),
-  CONSTRAINT cp PRIMARY KEY (numHotesse)
+  CONSTRAINT cPrimaryHotesse PRIMARY KEY (numHotesse)
 );
 
 CREATE TABLE ParleLangue (
   numHotesse NUMBER,
   libLangue VARCHAR2(100),
-  CONSTRAINT cp PRIMARY KEY (numHotesse, libLangue),
-  CONSTRAINT cf1 FOREIGN KEY (libLangue) REFERENCES Langue(libLangue),
-  CONSTRAINT cf2 FOREIGN KEY (numHotesse) rEFERENCES Hotesse(numHotesse)
+  CONSTRAINT cPrimaryParleLangue PRIMARY KEY (numHotesse, libLangue),
+  CONSTRAINT cForeign1ParleLangue FOREIGN KEY (libLangue) REFERENCES Langue(libLangue),
+  CONSTRAINT cForeign2ParleLangue FOREIGN KEY (numHotesse) rEFERENCES Hotesse(numHotesse)
 );
 
 --CONSTRAINTS
---
+++ drop.sql
@@ -0,0 +1,11 @@
+FROP TABLE ParleLangue;
+DROP TABLE Hotesse;
+DROP TABLE Langue;
+DROP TABLE Pilote;
+DROP TABLE Reservation;
+DROP TABLE Client;
+DROP TABLE PlaceVol;
+DROP TABLE Place;
+DROP TABLE Vol;
+DROP TABLE Avion;
+DROP TABLE Modele;

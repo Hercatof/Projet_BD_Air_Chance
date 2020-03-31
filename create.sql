bda2bd543292ee352dc6d4b938d2d76ff6372e54
Hercatof <56153246+Hercatof@users.noreply.github.com>
2020-03-31 19:38:22 +0200

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,87 +1,88 @@
 CREATE TABLE Modele (
-    nom_modele VARCHAR(100) NOT NULL,
-    nombre_pilotes_minimum INT,
-    rayon_maximum INT,
-    PRIMARY KEY (nom_modele)
+    nomModele VARCHAR2(100) NOT NULL,
+    nbPilotesMin NUMBER,
+    rayonMax NUMBER,
+    PRIMARY KEY (nomModele)
 )
 
 CREATE TABLE Avion (
-  numero_avion INT NOT NULL,
-  nom_modele VARCHAR(100) NOT NULL,
-  capacite INT,
-  nombre_places_eco INT,
-  nombre_places_premiere INT,
-  nombre_places_affaire INT,
-  PRIMARY KEY (numero_avion),
-  FOREIGN KEY (nom_modele) REFERENCES Modele(nom_modele)
+  numAvion NUMBER NOT NULL,
+  nomModele VARCHAR2(100) NOT NULL,
+  capacite NUMBER,
+  nbPlacesEco NUMBER,
+  nbPlacesPremiere NUMBER,
+  nbPlacesAffaire NUMBER,
+  PRIMARY KEY (numAvion),
+  FOREIGN KEY (nomModele) REFERENCES Modele(nomModele)
 )
 
 CREATE TABLE Vol (
-  numero_vol INT NOT NULL,
-  numero_avion INT NOT NULL,
-  aeroport_origine VARCHAR(100),
-  aeroport_destination VARCHAR(100),
-  horaire_date_depart DATE,
-  horaire_date_arrivee DATE,
-  distance_vol INT,
-  terminaison_vol BOOLEAN,
-  nombre_pilotes_minimum INT,
-  PRIMARY KEY (numero_vol),
-  FOREIGN KEY (numero_avion) REFERENCES Avion(numero_avion)
+  numVol NUMBER NOT NULL,
+  numAvion NUMBER NOT NULL,
+  origine VARCHAR2(100),
+  destination VARCHAR2(100),
+  heureDep TIME,
+  jourDep DATE,
+  heureAr TIME,
+  jourAr DATE,
+  distance NUMBER,
+  volTermine BOOLEAN,
+  nbPilotesMin NUMBER,
+  PRIMARY KEY (numVol),
+  FOREIGN KEY (numAvion) REFERENCES Avion(numAvion)
 )
 
 CREATE TABLE Place (
-  numero_place INT NOT NULL,
-  numero_avion INT NOT NULL,
-  classe VARCHAR(100),
-  position INT,
-  PRIMARY KEY (numero_place),
-  FOREIGN KEY (numero_avion) REFERENCES Avion(numero_avion)
+  numPlace NUMBER NOT NULL,
+  numAvion NUMBER NOT NULL,
+  classe VARCHAR2(100),
+  position NUMBER,
+  PRIMARY KEY (numPlace),
+  FOREIGN KEY (numAvion) REFERENCES Avion(numAvion)
 )
 
-CREATE TABLE Place_Vol (
-  numero_vol INT NOT NULL,
-  numero_place INT NOT NULL,
-  PRIMARY KEY (numero_vol,numero_place),
-  FOREIGN KEY (numero_vol) REFERENCES Vol(numero_vol),
-  FOREIGN KEY (numero_place) REFERENCES Place(numero_place)
+CREATE TABLE PlaceVol (
+  numVol NUMBER NOT NULL,
+  numPlace NUMBER NOT NULL,
+  PRIMARY KEY (numVol,numero_place),
+  FOREIGN KEY (numVol) REFERENCES Vol(numVol),
+  FOREIGN KEY (numPlace) REFERENCES Place(numPlace)
 )
 
 
 CREATE TABLE Client (
-  numero_client INT,
-  numero_passeport INT,
-  nom VARCHAR(100),
-  prenom VARCHAR(100),
-  adresse_numero INT,
-  adresse_rue VARCHAR(100),
-  adresse_code_postal INT,
-  adresse_ville VARCHAR(100),
-  adresse_pays VARCHAR(100),
-  PRIMARY KEY (numero_client)
+  numClient NUMBER,
+  numPasseport NUMBER,
+  nom VARCHAR2(100),
+  prenom VARCHAR2(100),
+  adrNum NUMBER,
+  adrRue VARCHAR2(100),
+  adrCodePostal NUMBER,
+  adrVille VARCHAR2(100),
+  adrPays VARCHAR2(100),
+  PRIMARY KEY (numClient)
 )
 
 CREATE TABLE Reservation (
-  numero_reservation INT NOT NULL,
-  numero_client INT NOT NULL
-  numero_vol INT NOT NULL,
-  numero_place INT NOT NULL,
-  date_reservation DATE,
+  numReservation NUMBER NOT NULL,
+  numClient NUMBER NOT NULL
+  numVol NUMBER NOT NULL,
+  numPlace NUMBER NOT NULL,
+  dateReservation DATE,
   prix DECIMAL,
-  PRIMARY KEY (numero_reservation),
-  FOREIGN KEY (numero_client) REFERENCES Client(numero_client),
-  FOREIGN KEY (numero_vol, numero_place) REFERENCES Place_Vol(numero_vol, numero_place)
+  PRIMARY KEY (numReservation),
+  FOREIGN KEY (numClient) REFERENCES Client(numClient),
+  FOREIGN KEY (numVol, numPlace) REFERENCES PlaceVol(numVol, numPlace)
 )
 
---TO CHECK
 CREATE TABLE Pilote (
-  numero_personnel INT NOT NULL,
-  nom VARCHAR(100),
-  prenom VARCHAR(100),
-  adresse_numero INT,
-  nombre_vols_total VARCHAR(100),
-  nombre_heures_vol_total INT,
-  PRIMARY KEY (numero_personnel)
+  numPilote NUMBER NOT NULL,
+  nom VARCHAR2(100),
+  prenom VARCHAR2(100),
+  adrNum NUMBER,
+  nbVols VARCHAR2(100),
+  nbHeuresVol NUMBER,
+  PRIMARY KEY (numPilote)
 )
 
 CREATE TABLE Langue (
@@ -90,13 +91,12 @@ CREATE TABLE Langue (
 )
 
 CREATE TABLE Hotesse (
-  numero_personnel INT NOT NULL,
-  nom VARCHAR(100),
-  prenom VARCHAR(100),
-  adresse_numero INT,
-  nombre_heures_vol_total INT,
-  PRIMARY KEY (numero_personnel)
-  --est qualifié pour un avion
+  numHotesse NUMBER NOT NULL,
+  nom VARCHAR2(100),
+  prenom VARCHAR2(100),
+  adrNum NUMBER,
+  nbHeuresVol NUMBER,
+  PRIMARY KEY (numHotesse)
 )
 
 --CONSTRAINTS
